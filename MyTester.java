cd96d58ddebf90359f6a3bd1cac3656e302e0a60
rjcapuno <rjcapuno04@gmail.com>
2016-02-19 22:51:34 +0800

    updated MyTester

--- MyTester.java
+++ MyTester.java
@@ -6,16 +6,36 @@ import org.junit.Test;
 public class MyTester {
 
 	@Test
-	public void testNfactorial(int n) {
+	public void testNfactorial() {
+		long actual;
 		MyCalculator calc = new MyCalculator();
-		int actual;
+		
 		actual = calc.nfactorial(5);
 		assertEquals("test", 120, actual);
 	}
 
 	@Test
 	public void testBinarySearch() {
-		//fail("Not yet implemented");
+		long actual;
+		int[] test = new int[5];
+		
+		test[0] = 1;
+		test[1] = 2;
+		test[2] = 3;
+		test[3] = 4;
+		test[4] = 5;
+		MyCalculator calc = new MyCalculator();
+		
+		actual = calc.binarySearch(test, 1);
+		assertEquals("test", 0, actual);
+		actual = calc.binarySearch(test, 2);
+		assertEquals("test", 1, actual);
+		actual = calc.binarySearch(test, 3);
+		assertEquals("test", 2, actual);
+		actual = calc.binarySearch(test, 4);
+		assertEquals("test", 3, actual);
+		actual = calc.binarySearch(test, 5);
+		assertEquals("test", 4, actual);
 	}
 
 }
